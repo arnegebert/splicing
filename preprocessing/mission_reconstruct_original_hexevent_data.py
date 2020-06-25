8aa83a380d153660a188222034c326d7ac34c18c
Arne Gebert <arne.gebert@student.uni-tuebingen.de>
2020-06-25 11:38:10 +0200

    Ye b o i

--- preprocessing/mission_reconstruct_original_hexevent_data.py
+++ preprocessing/mission_reconstruct_original_hexevent_data.py
@@ -7,7 +7,7 @@ startt = time.time()
 data_path_decoded = '../data/distributed'
 data_path_origin = '../data/send_to_me_DSC'
 # data_path_origin = '../data/hexevent'
-lifehack = 1000000
+lifehack = 60000000000000000
 
 # decoded data
 with open(f'{data_path_decoded}/decoded_cons_data_class.csv') as f:
@@ -18,12 +18,12 @@ decoded_cons = np.array(decoded_cons)[:lifehack]
 with open(f'{data_path_decoded}/decoded_cas_data_high_class.csv') as f:
     reader = csv.reader(f, delimiter='\t')
     hx_cas_data = list(reader)
-hx_cas_data = np.array(hx_cas_data)[:lifehack]
+hx_cas_data = np.array(hx_cas_data)[:lifehack//2]
 
 with open(f'{data_path_decoded}/decoded_cas_data_low_class.csv') as f:
     reader = csv.reader(f, delimiter='\t')
     lx_cas_data = list(reader)
-lx_cas_data = np.array(lx_cas_data)[:lifehack]
+lx_cas_data = np.array(lx_cas_data)[:lifehack//2]
 decoded_cas = np.concatenate((hx_cas_data, lx_cas_data))
 
 # original exons
@@ -69,9 +69,11 @@ def get_used_exons(decoded, original):
     used_exons = []
     loaded_chrom = 1
     chrom_seq = load_chrom_seq(loaded_chrom)
-    hasht = dict()
-    for i, seq in enumerate(decoded[:, 0]):
-        hasht[seq] = i
+    hasht_start, hasht_end = dict(), dict()
+    for i, (seq_start, seq_end) in enumerate(decoded[:, :2]):
+        if seq_start in hasht_start: print(f'decoded sequence {i} is duplicate')
+        hasht_start[seq_start] = i
+        hasht_end[seq_end] = i
 
     print('Start of iteration through lines')
     for i, line in enumerate(original):
@@ -95,28 +97,37 @@ def get_used_exons(decoded, original):
         window_around_end = chrom_seq[end - exons_bef_end:end + introns_after_end].upper()
         if strand == '+':
             pos += 1
-            in_there = window_around_start in hasht
+            seq = window_around_start
+            in_there = seq in hasht_start
+            # decoded_idx = hasht_start[seq]
         if strand == '-':
-            continue
             neg += 1
+            seq = reverse_complement(window_around_end[::-1])
+            in_there = seq in hasht_start
+            # decoded_idx = hasht_start[seq]
+            # if in_there2: print('reeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
             # window_around_start = window_around_start[70:]
-            cpy = window_around_start[105:]
-            cpy = cpy[::-1]
-            cpy = reverse_complement(cpy)
-            # window_around_start = window_around_start[::-1]
-            # window_around_start = reverse_complement(window_around_start)
-            in_there = any([cpy in seq for seq in decoded[:, 0]])
-            # match beginning at index 62
-            # 31 the other time
-            # before 62, also matches sequence before
-            # therefore, extracting moved by 62 nts + reversed + reverse complemented????
+            # cpy = window_around_end
+            # cpy = cpy[::-1]
+            # cpy = reverse_complement(cpy)
+            # in_there = any([cpy in seq for seq in decoded[:, 0]])
+            # if in_there:
+            #     print(f'Offset index: {decoded[[cpy in seq for seq in decoded[:, 0]].index(True),0].find(cpy)}')
         if in_there: # +: 20286, -: 42079
-            decoded_idx = hasht[window_around_start]
+            # decoded_idx = hasht_start[window_around_start]
+            decoded_idx = hasht_start[seq]
+            print(f'Original exon {i} matched to decoded exon: {decoded_idx}')
             l1, l2, l3 = decoded[decoded_idx, 2:5]
+            # l1, l2, l3 = 0, 0,0
             used_exons.append((chrom, strand, start, end, count, skip, constit_level, l1, l2, l3))
-            print(i)
-            # print(f'Offset index: {decoded[[cpy in seq for seq in decoded[:, 0]].index(True),0].find(cpy)}')
     return used_exons
+
+# 35958:
+# chr4	+	118544315	118544404	9	0	0	0	0	1.000	1.000	1.000	1.000
+# seq: 'TAGTGATCTCAGTAGTTATACAATACAGGGGTAATATTTTAATATCTACTTTTTTCTTTGGCTGTTTCAGGTCATGGGAGATAATCTGCTGCTGTCATCCGTCTTTCAGTTCTCTAAGAAGATAAGACAATCTATAGATA'
+# 12990:
+# chr1	-	243136142	243136231	27	0	0	0	0	1.000	1.000	1.000	1.000	0	#	0	#	0
+# seq: 'TAGTGATCTCAGTAGTTATACAATACAGGGGTAATATTTTAATATCTACTTTTTTCTTTGGCTGTTTCAGGTCATGGGAGATAATCTGCTGCTGTCATCCGTCTTTCAGTTCTCTAAGAAGATAAGACAATCTATAGATA'
 
 # wowww -- 200-times speed up through hashing :>
 # original: 64902/65262 +/- cons exons
@@ -140,6 +151,9 @@ used_cass_exons = get_used_exons(decoded_cas, original_cass)
 with open('../data/dsc_reconstruction_junction/cass_exons.csv', 'w') as f:
     for chrom, strand, start, end, count, skip, constit_level, l1, l2, l3 in used_cass_exons:
         f.write(f'{chrom}\t{strand}\t{start}\t{end}\t{count}\t{skip}\t{constit_level}\t{l1}\t{l2}\t{l3}\n')
+
+print(f'Found {len(used_cass_exons)} matching cassette exons')
+print(f'Wanted to find {len(decoded_cas)} cassette exons')
 
 print(f'Found {len(used_cons_exons)} matching constitutive exons')
 print(f'Wanted to find {len(decoded_cons)} constitutive exons')
