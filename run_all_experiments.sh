5e5b4decfffd59c4a581c1a6f7239c2c79734668
Arne Gebert <arne.gebert@student.uni-tuebingen.de>
2020-08-21 22:30:59 +0200

    idk mam

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-RUNID='final'
+RUNID='test'
 for configname in configs/*.json; do
   echo "--------------------------------------------------------"
   echo "Executing experiment $configname" ;
   trap 'kill -TERM $PID' TERM INT
-  python train.py -rid RUNID -c $configname &> results/$configname &
+  python train.py -rid $RUNID -c $configname
   PID=$!
   wait $PID
   trap - TERM INT
