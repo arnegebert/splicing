6d2f6e18e48a3c413e4827b91802fcd72d6ee7c9
Arne Gebert <arne.gebert@student.uni-tuebingen.de>
2020-08-21 20:12:10 +0200

    still felt cute; deleted 4 seq and embedded data loaders & trainers

--- data_loader/__init__.py
+++ data_loader/__init__.py
@@ -1,11 +1,7 @@
 from .GTEx_EmbeddedDataLoader import *
 
 from .Comparison_DataLoader import *
-from .Comparison_EmbeddedDataLoader import *
 
 from .Vanilla_DataLoader import *
-from .Vanilla_EmbeddedDataLoader import *
-from .Vanilla_4_DataLoader import *
 
 from .HEXEvent2Vanilla_DataLoader import *
-from .utils import *
--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,9 +1,5 @@
 from .GTEx_D2V_Trainer import *
 
 from .Comparison_Trainer import *
-from .Comparison_EmbeddedDataTrainer import *
 
 from .Vanilla_Trainer import *
-from .Vanilla_EmbeddedDataTrainer import *
-from .Vanilla_4_EmbeddedDataTrainer import *
-from .Vanilla_4_Trainer import *
