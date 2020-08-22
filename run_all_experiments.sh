c42e5cbde431a4f9824e15fc8eaea300635719e2
Arne Gebert <arne.gebert@student.uni-tuebingen.de>
2020-08-22 15:28:41 +0200

    dropout and overnight results

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-RUNID='test'
+RUNID='overnight'
 for configname in configs/*.json; do
   echo "--------------------------------------------------------"
   echo "Executing experiment $configname" ;
   trap 'kill -TERM $PID' TERM INT
   python train.py -rid $RUNID -c $configname
+  #python train.py -rid $RUNID -cv true -c $configname
   PID=$!
   wait $PID
   trap - TERM INT
